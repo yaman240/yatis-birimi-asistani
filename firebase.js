e96575728085dd05a673c704bec37d5eabc1b4cf
yaman240 <yaman615@gmail.com>
2026-07-27 11:22:01 +0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,14 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDvAPSMf__Z1qGDezD5V5cagV0t1tDybTU",
-  authDomain: "yatis-birim-asistani.firebaseapp.com",
-  projectId: "yatis-birim-asistani",
-  storageBucket: "yatis-birim-asistani.firebasestorage.app",
-  messagingSenderId: "366593697008",
-  appId: "1:366593697008:web:68c029a33edf9fc7490e35"
-};
-
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+const firebaseConfig={apiKey:"AIzaSyDvAPSMf__Z1qGDezD5V5cagV0t1tDybTU",authDomain:"yatis-birim-asistani.firebaseapp.com",projectId:"yatis-birim-asistani",storageBucket:"yatis-birim-asistani.firebasestorage.app",messagingSenderId:"366593697008",appId:"1:366593697008:web:68c029a33edf9fc7490e35"};
+const app=initializeApp(firebaseConfig);export const db=getFirestore(app);
