a0490a0c9adcf023df6215534c814cb8a4ff8d3e
yaman240 <yaman615@gmail.com>
2026-07-29 10:44:41 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "yatis-asistani-v8";
-const CORE_FILES = ["./","./index.html","./style.css?v=8","./app.js?v=8","./firebase.js?v=8","./manifest.json","./icon-192.png","./icon-512.png"];
+const CACHE_NAME = "yatis-asistani-v9";
+const CORE_FILES = ["./","./index.html","./style.css?v=9","./app.js?v=9","./firebase.js?v=9","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE_FILES)))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match("./index.html"))))});
